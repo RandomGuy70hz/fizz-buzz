c1e2b60f3d983aa871cb04b7fe4b4c16f876de00
Random Guy 70hz <codymoores@live.com>
2022-04-18 01:20:13 -0230

    fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,26 @@
+# Fizz Buzz Program
+import re # import regex package
+
+def fizz_buzz(input):       # our fizz buzz function takes an input
+	if input % 3 == 0:      # if input is evenly divided
+		result = "Fizz"     # result is equal to Fizz
+	else:                   # if input is not evenly divided
+		result = "Buzz"     # result is equal to Buzz
+	return result           # return the result  variable
+
+
+def greet():                # main function called at start
+    freshstart = True       # loop control variable
+    print('Input a number') # print to console
+
+    while(freshstart):      # while control is true
+        number = input()    # store input as variable called number
+
+        if re.match("[^\d]", number):       # if regex  matches input of non-integer
+            print('Try Again')              # print 'Try Again'
+        else:                               # if regex detects an integer
+            print(fizz_buzz(int(number)))   # execute FizzBuzz function
+            freshstart = False              # ends loop and stops program
+
+# Main function call
+greet() 
